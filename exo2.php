33b2e4c69b0f60c71f779eb79586301f943d043c
Alison-Denis <74008946+Alison-Denis@users.noreply.github.com>
2020-12-07 14:04:26 +0100

    Add files via upload

---
+++ exo2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>PhpPart1Exo2</title>
+</head>
+
+<body>
+
+<?php
+$lastName= "Denis";
+$firsttName= "Alison";
+$age= "26";
+
+echo "$lastName $firsttName est âgé(e) de $age ans.";
+?>
+
+</body>
+</html>
